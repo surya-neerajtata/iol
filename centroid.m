9c9453718998c279150ef07bdd583a62a1eac105
surya-neerajtata <neeraj.tata@kcl.ac.uk>
2018-01-24 15:49:17 +0000

    Update centroid.m

--- centroid.m
+++ centroid.m
@@ -2,6 +2,7 @@ function y = centroid(img)
 a = imread(img);
 bw = a < 60;
 dw = [0,0];
+
 for i = 1:512
 	for j = 1:512
 		if bw(i,j) == 0
@@ -11,8 +12,15 @@ for i = 1:512
 	end
 end
 dw = dw(2:end,:);
-plot(513-dw(:,1),513-dw(:,2),'*');
-y =[513-((max(dw(:,1))-min(dw(:,1)))/2+min(dw(:,1))),513-((max(dw(:,2))-min(dw(:,2)))/2+min(dw(:,2)))]
-hold on
-plot(y(:,1),y(:,2),'*');
-hold off
+y =[513-((max(dw(:,1))-min(dw(:,1)))/2+min(dw(:,1))),513-((max(dw(:,2))-min(dw(:,2)))/2+min(dw(:,2)))];
+imshow(a);
+gp = ginput(1)
+res = vertcat(y,gp)
+colour = {'red', 'green'};
+RGB = insertMarker(a, res, 'color' ,colour);
+imshow(RGB)
+disp(['Manual Input is ',num2str(gp),'. The Algorithm code gives ', num2str(y)])
+% hold on
+% lw = plot(y(:,1),y(:,2),'*');
+% c = imfuse(a,y,'blend');
+% imshow(c)
