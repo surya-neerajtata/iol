2832805d84788fa7b55067abda70e904dd140d4a
surya-neerajtata <neeraj.tata@kcl.ac.uk>
2018-01-29 12:37:45 +0000

    Update centroid.m
    Using CircleFitByPratt, we calculated the radius and centre for the circle of best fit of the boundary pixels.
    
     Circle fit by Pratt:
         V. Pratt, "Direct least-squares fitting of algebraic surfaces",
         Computer Graphics, Vol. 21, pages 145-152 (1987)

--- centroid.m
+++ centroid.m
@@ -1,8 +1,8 @@
-function y = centroid(img)
+function y = centroid(img);
 a = imread(img);
 bw = a < 60;
 dw = [0,0];
-
+% imshow(bw)
 for i = 1:512
 	for j = 1:512
 		if bw(i,j) == 0
@@ -12,14 +12,30 @@ for i = 1:512
 	end
 end
 dw = dw(2:end,:);
-y =[513-((max(dw(:,1))-min(dw(:,1)))/2+min(dw(:,1))),513-((max(dw(:,2))-min(dw(:,2)))/2+min(dw(:,2)))];
-imshow(a);
-gp = ginput(1)
-res = vertcat(y,gp)
-colour = {'red', 'green'};
-RGB = insertMarker(a, res, 'color' ,colour);
-imshow(RGB)
-disp(['Manual Input is ',num2str(gp),'. The Algorithm code gives ', num2str(y)])
+y = boundary(dw);
+circlex = dw(y,1);
+circley = dw(y,2);
+circlep = horzcat(circlex,circley);
+cb = CircleFitByPratt(circlep);
+cx = cb(1,1);
+cy = cb(1,2);
+y = [cy,cx];
+
+% imshow(a);
+% gp = ginput(1)
+% res = vertcat(c,gp);
+% colour = {'red', 'green'};
+% RGB = insertMarker(a, res, 'color' ,colour);
+% imshow(RGB)
+% y =[((max(dw(:,1))-min(dw(:,1)))/2+min(dw(:,1))),((max(dw(:,2))-min(dw(:,2)))/2+min(dw(:,2)))];
+% imshow(a);
+% Max = [min(dw(:,1)),max(dw(:,1));min(dw(:,2)),max(dw(:,2))]
+% gp = ginput(1);
+% res = vertcat(y,gp);
+% colour = {'red', 'green'};
+% RGB = insertMarker(a, res, 'color' ,colour);
+% imshow(RGB)
+% disp(['Manual Input is ',num2str(gp),'. The Algorithm code gives ', num2str(y)])
 % hold on
 % lw = plot(y(:,1),y(:,2),'*');
 % c = imfuse(a,y,'blend');
